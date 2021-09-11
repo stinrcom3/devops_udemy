b6c38c518a4fd8ee44399380626d416f1a413a21
stinrcom3 <srtinrcom3@gmail.com>
2021-09-10 21:40:25 -0500

    agregar cambio en java

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,16 +1,35 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org. postgresql.translation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class BasicApplicationTests {
 
-	@Test
-	public void contextLoads() {
+ @Test
+ public void contextLoads() {
+	    Stringgmessage= *default message cambio 11*;
+	    Assert.assertNotNull)(message);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
